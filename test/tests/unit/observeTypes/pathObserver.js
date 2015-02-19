14cf5d96f853fd17a091959ff75e7bf35e840e41
Shane Connon <shane.connon@gmail.com>
2015-02-19 12:37:02 -0700

    tests

--- test/tests/unit/observeTypes/pathObserver.js
+++ test/tests/unit/observeTypes/pathObserver.js
@@ -7,16 +7,36 @@ module("obsjs.observeTypes.pathObserver", {
 
 var pathObserver = obsjs.observeTypes.pathObserver;
 
-testUtils.testWithUtils("execute", null, false, function(methods, classes, subject, invoker) {
+testUtils.testWithUtils("_execute", "no cancel", false, function(methods, classes, subject, invoker) {
     // arrange
+	subject.val = 11;
     subject.forObject = {
         aa: {bb: {cc: 22}}
     };
     subject.path = ["aa", "bb", "cc"];
     
     // act
-    invoker();
+    var output = invoker();
     
     // assert
     strictEqual(subject.val, 22);
+    strictEqual(output.arguments.length, 2);
+    strictEqual(output.arguments[0], 11);
+    strictEqual(output.arguments[1], 22);
+    ok(!output.cancel);
+});
+
+testUtils.testWithUtils("_execute", "no cancel", false, function(methods, classes, subject, invoker) {
+    // arrange
+	subject.val = 11;
+    subject.forObject = {
+        aa: {bb: {cc: 11}}
+    };
+    subject.path = ["aa", "bb", "cc"];
+    
+    // act
+    var output = invoker();
+    
+    // assert
+    ok(output.cancel);
 });
