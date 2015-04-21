6915150fa6d26751e4a719a11b3d2db0421e6001
Shane Connon <shane.connon@gmail.com>
2015-04-20 21:57:32 -0600

    docs

--- src/utils/executeCallbacks.js
+++ src/utils/executeCallbacks.js
@@ -3,7 +3,7 @@
 Class("busybody.utils.executeCallbacks", function () {
 	
 	var executeCallbacks = busybody.disposable.extend(function executeCallbacks() {
-		///<summary>Base class for objects with an execute function which executes a list of callbacks<summary>
+		///<summary>Base class for objects with an execute(...) function which executes a list of callbacks<summary>
 		
 		if (this.constructor === executeCallbacks) throw "You cannot create an instance of an abstract class";
 		
@@ -13,6 +13,11 @@ Class("busybody.utils.executeCallbacks", function () {
 	});
 	
 	executeCallbacks.prototype.addCallback = function (callback) {
+		///<summary>Add a callback<summary>
+		///<param name="callback" type="Function">The callback<param>
+		///<param name="property" type="String">The property<param>
+		///<returns type="busybody.disposable">A dispose object<param>
+		
 		var op = busybody.utils.obj.addWithDispose(this.callbacks, callback);
 		this.registerDisposable(op);
 		
@@ -20,11 +25,15 @@ Class("busybody.utils.executeCallbacks", function () {
 	};
         
     executeCallbacks.prototype._execute = function() {
+		///<summary>Abstract. Execute and return argumets for the callbacks<summary>
+		///<returns type="Object">Arguments for the callbacks in the form of { cancel: true | false, arguments: [] }<param>
+		
 		throw "Abstract methods must be implemented";
-		// returns { cancel: true | false, arguments: [] }
 	};
 	
     executeCallbacks.prototype.execute = function() {
+		///<summary>Execute all callbacks<summary>
+		
 		var args = this._execute();
 		
 		if (args && !args.cancel)
@@ -34,10 +43,9 @@ Class("busybody.utils.executeCallbacks", function () {
     };
     
     executeCallbacks.prototype.dispose = function () {
-		this._super();
+		///<summary>Dispose<summary>
 		
-		// clear __executePending timeout?
-		
+		this._super();		
 		this.callbacks.length = 0;
     };
 	
